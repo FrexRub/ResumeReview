68a15203e408ea8ae2e5f5ef4f29fb7c1e25d4eb
Станислав Рубцов <frexrub@gmail.com>
2026-07-28 16:24:34 +0300

    add alembic

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,6 +1,4 @@
-from .airport import Airport
 from .base import Base
-from .comment import AirportComment
 from .user import User
 
-__all__ = ["Base", "User", "Airport", "AirportComment"]
+__all__ = ["Base", "User"]
